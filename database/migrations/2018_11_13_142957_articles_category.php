eec46ef51153257c4a2ba1a471127098c1d3ee85
youge <46914685@qq.com>
2018-11-13 22:46:19 +0800

    category

---
+++ database/migrations/2018_11_13_142957_articles_category.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ArticlesCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name',100)->unique('category_name')->default('')->comment('栏目名称');
+            $table->unsignedInteger('parent_id')->default(0)->comment('父级栏目id');
+            $table->unsignedTinyInteger('is_expand')->default(0)->comment('是否展开,0否1是');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
