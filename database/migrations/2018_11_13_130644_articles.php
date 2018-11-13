63bd1e7cbf4c4723e3d33c75b53baf4d2a9a25ef
youge <46914685@qq.com>
2018-11-13 21:17:16 +0800

    sd

---
+++ database/migrations/2018_11_13_130644_articles.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('creeated_at');
+            $table->unsignedInteger('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
