1f019ba48c8b13864f2c76c3db86356c86aa0418
youge <46914685@qq.com>
2018-11-13 23:00:32 +0800

    dsd

--- app/Http/Controllers/admin/IndexController.php
+++ app/Http/Controllers/admin/IndexController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ContentController extends Controller
-{
-    //
+class IndexController extends Controller{
+
+    public function index(){
+
+        return view('admin.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'admin','namespace'=>'admin'],function(){
+    Route::get('/','AdminHomeController@index');
+});
