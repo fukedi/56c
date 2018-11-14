42ffa1b7654f09b3f40bc983a4d10d41a5a300fa
youge <46914685@qq.com>
2018-11-14 12:33:32 +0800

    11

--- resources/views/admin/article/categoryList.blade.php
+++ resources/views/admin/article/categoryList.blade.php
@@ -73,26 +73,40 @@
                 <div class="x_panel">
                     <div class="x_content">
                         <br />
-                        <el-tree
-                                :data="data"
-                                :props="defaultProps"
-                                accordion
-                                @node-click="handleNodeClick">
-                            <span>
-          <el-button
-                  type="text"
-                  size="mini"
-                  @click="() => append(data)">
-            Append
-          </el-button>
-          <el-button
-                  type="text"
-                  size="mini"
-                  @click="() => remove(node, data)">
-            Delete
-          </el-button>
-        </span>
-                        </el-tree>
+                        {{--<el-tree--}}
+                                {{--:data="data"--}}
+                                {{--:props="defaultProps"--}}
+                                {{--accordion--}}
+                                {{--@node-click="handleNodeClick">--}}
+                            {{--<span>--}}
+          {{--<el-button--}}
+                  {{--type="text"--}}
+                  {{--size="mini"--}}
+                  {{--@click="() => append(data)">--}}
+            {{--Append--}}
+          {{--</el-button>--}}
+          {{--<el-button--}}
+                  {{--type="text"--}}
+                  {{--size="mini"--}}
+                  {{--@click="() => remove(node, data)">--}}
+            {{--Delete--}}
+          {{--</el-button>--}}
+        {{--</span>--}}
+                        {{--</el-tree>--}}
+
+                        <div class="block">
+                            <p>使用 render-content</p>
+                            <el-tree
+                                    :data="data4"
+                                    show-checkbox
+                                    node-key="id"
+                                    default-expand-all
+                                    :expand-on-click-node="false"
+                                    :render-content="renderContent">
+                            </el-tree>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
@@ -110,51 +124,97 @@
         new Vue({
             el: '#app',
             data: function() {
+                // return {
+                //     data: [{
+                //         label: '一级 1',
+                //         children: [{
+                //             label: '二级 1-1',
+                //             children: [{
+                //                 label: '三级 1-1-1'
+                //             }]
+                //         }]
+                //     }, {
+                //         label: '一级 2',
+                //         children: [{
+                //             label: '二级 2-1',
+                //             children: [{
+                //                 label: '三级 2-1-1'
+                //             }]
+                //         }, {
+                //             label: '二级 2-2',
+                //             children: [{
+                //                 label: '三级 2-2-1'
+                //             }]
+                //         }]
+                //     }, {
+                //         label: '一级 3',
+                //         children: [{
+                //             label: '二级 3-1',
+                //             children: [{
+                //                 label: '三级 3-1-1'
+                //             }]
+                //         }, {
+                //             label: '二级 3-2',
+                //             children: [{
+                //                 label: '三级 3-2-1'
+                //             }]
+                //         }]
+                //     }],
+                //     defaultProps: {
+                //         children: 'children',
+                //         label: 'label'
+                //     }
+                // };
+
+                const data = [{
+                    id: 1,
+                    label: '一级 1',
+                    children: [{
+                        id: 4,
+                        label: '二级 1-1',
+                        children: [{
+                            id: 9,
+                            label: '三级 1-1-1'
+                        }, {
+                            id: 10,
+                            label: '三级 1-1-2'
+                        }]
+                    }]
+                }, {
+                    id: 2,
+                    label: '一级 2',
+                    children: [{
+                        id: 5,
+                        label: '二级 2-1'
+                    }, {
+                        id: 6,
+                        label: '二级 2-2'
+                    }]
+                }, {
+                    id: 3,
+                    label: '一级 3',
+                    children: [{
+                        id: 7,
+                        label: '二级 3-1'
+                    }, {
+                        id: 8,
+                        label: '二级 3-2'
+                    }]
+                }];
                 return {
-                    data: [{
-                        label: '一级 1',
-                        children: [{
-                            label: '二级 1-1',
-                            children: [{
-                                label: '三级 1-1-1'
-                            }]
-                        }]
-                    }, {
-                        label: '一级 2',
-                        children: [{
-                            label: '二级 2-1',
-                            children: [{
-                                label: '三级 2-1-1'
-                            }]
-                        }, {
-                            label: '二级 2-2',
-                            children: [{
-                                label: '三级 2-2-1'
-                            }]
-                        }]
-                    }, {
-                        label: '一级 3',
-                        children: [{
-                            label: '二级 3-1',
-                            children: [{
-                                label: '三级 3-1-1'
-                            }]
-                        }, {
-                            label: '二级 3-2',
-                            children: [{
-                                label: '三级 3-2-1'
-                            }]
-                        }]
-                    }],
-                    defaultProps: {
-                        children: 'children',
-                        label: 'label'
-                    }
-                };
+                    data4: JSON.parse(JSON.stringify(data)),
+                    data5: JSON.parse(JSON.stringify(data))
+                }
+
+
             },
             methods: {
                 handleNodeClick(data) {
                     console.log(data);
+                },
+
+                renderContent(h, { node, data, store }) {
+                    return ('<span class="custom-tree-node"><span>{node.label}</span><span><el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button><el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button></span></span>');
                 }
             }
         })
--- resources/views/admin/layouts.blade.php
+++ resources/views/admin/layouts.blade.php
@@ -2,7 +2,6 @@
 <html lang="zh-cn">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,22 +11,10 @@
     @section('css')
     <!-- Bootstrap -->
     <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{asset('vendors/nprogress/nprogress.css')}}" rel="stylesheet">
-    <!-- iCheck -->
-    <link href="{{asset('vendors/iCheck/skins/flat/green.css')}}" rel="stylesheet">
-
-    <!-- bootstrap-progressbar -->
-    <link href="{{asset('vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet">
-    <!-- JQVMap -->
-    <link href="{{asset('vendors/jqvmap/dist/jqvmap.min.css')}}" rel="stylesheet"/>
-    <!-- bootstrap-daterangepicker -->
-    <link href="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
-
-    <!-- Custom Theme Style -->
-    <link href="{{asset('css/custom.min.css')}}" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+        <!-- Custom Theme Style -->
+        <link href="{{asset('css/custom.min.css')}}" rel="stylesheet">
         @show
 </head>
 
@@ -37,18 +24,14 @@
         <div class="col-md-3 left_col">
             @include('admin.common.left')
         </div>
-
     <!-- top navigation -->
        @include('admin.common.header')
-
         <!-- /top navigation -->
-
         <!-- page content -->
         <div class="right_col" role="main">
             @yield('content')
         </div>
         <!-- /page content -->
-
         <!-- footer content -->
         <footer>
             <div class="pull-right">
@@ -64,43 +47,8 @@
 @section('script')
 <!-- jQuery -->
 <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
-<!-- Bootstrap -->
+{{--<!-- Bootstrap -->--}}
 <script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-<!-- FastClick -->
-<script src="{{asset('vendors/fastclick/lib/fastclick.js')}}"></script>
-<!-- NProgress -->
-<script src="{{asset('vendors/nprogress/nprogress.js')}}"></script>
-<!-- Chart.js -->
-<script src="{{asset('vendors/Chart.js/dist/Chart.min.js')}}"></script>
-<!-- gauge.js -->
-<script src="{{asset('vendors/gauge.js/dist/gauge.min.js')}}"></script>
-<!-- bootstrap-progressbar -->
-<script src="{{asset('vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
-<!-- iCheck -->
-<script src="{{asset('vendors/iCheck/icheck.min.js')}}"></script>
-<!-- Skycons -->
-<script src="{{asset('vendors/skycons/skycons.js')}}"></script>
-<!-- Flot -->
-<script src="{{asset('vendors/Flot/jquery.flot.js')}}"></script>
-<script src="{{asset('vendors/Flot/jquery.flot.pie.js')}}"></script>
-<script src="{{asset('vendors/Flot/jquery.flot.time.js')}}"></script>
-<script src="{{asset('vendors/Flot/jquery.flot.stack.js')}}"></script>
-<script src="{{asset('vendors/Flot/jquery.flot.resize.js')}}"></script>
-<!-- Flot plugins -->
-<script src="{{asset('vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
-<script src="{{asset('vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
-<script src="{{asset('vendors/flot.curvedlines/curvedLines.js')}}"></script>
-<!-- DateJS -->
-<script src="{{asset('vendors/DateJS/build/date.js')}}"></script>
-<!-- JQVMap -->
-<script src="{{asset('vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
-<script src="{{asset('vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
-<script src="{{asset('vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
-<!-- bootstrap-daterangepicker -->
-<script src="{{asset('vendors/moment/min/moment.min.js')}}"></script>
-<script src="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
-
-<!-- Custom Theme Scripts -->
 <script src="{{asset('js/custom.min.js')}}"></script>
 @show
 </body>
